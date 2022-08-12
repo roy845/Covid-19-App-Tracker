b410af2a55c10e7507d045159b36405255d3f1d9
roy845 <royatali94@gmail.com>
2022-08-12 10:23:16 +0300

    removed continents api url

--- app/src/main/java/com/example/finalproject/ContinentsViewModel.java
+++ app/src/main/java/com/example/finalproject/ContinentsViewModel.java
@@ -71,7 +71,7 @@ public class ContinentsViewModel  extends AndroidViewModel {
 
     private ArrayList<Continent> parseContinents(Context context) {
 
-        String urlGlobalStats = "https://disease.sh/v3/covid-19/continents";
+        String urlGlobalStats = "";
         StringRequest globalStatsRequest = new StringRequest(Request.Method.GET, urlGlobalStats,
                 new Response.Listener<String>() {
                     @Override
