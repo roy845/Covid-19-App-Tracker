69067b1bc1f8649a9fc9551da5fc7e7505b90734
roy845 <royatali94@gmail.com>
2022-08-12 10:33:52 +0300

    fixed countries url api

--- app/src/main/java/com/example/finalproject/CountriesViewModel.java
+++ app/src/main/java/com/example/finalproject/CountriesViewModel.java
@@ -91,7 +91,7 @@ public class CountriesViewModel extends AndroidViewModel {
 
     public  ArrayList<Country> parseCountries(Context context) {
 
-        String urlGlobalStats = "";
+        String urlGlobalStats = "https://disease.sh/v3/covid-19/countries";
         StringRequest globalStatsRequest = new StringRequest(Request.Method.GET, urlGlobalStats,
                 new Response.Listener<String>() {
 
